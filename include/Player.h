3a3fe4f69d47a239c4f3aff58a4badae3764890c
Boskaa <andrea.boscarin@gmail.com>
2023-12-21 21:33:03 +0100

    Added some Player.cpp definitions

--- include/Player.h
+++ include/Player.h
@@ -1,15 +1,32 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "Tile.h"
+#include <vector>
+#include <random>
+#include <ctime>
+
 class Player {
     protected:
         int balance;
         int position;
+        char id;
+        std::vector <Tile*> ownedTiles;
+        
 
     public:
         Player(void) {};
+        Player(int amount, int pos, char idnum);
+        Player(const Player& other);
+
         void print(void) {};
         int getBalance(void) const { return this->balance; };
+        char getId(void) const { return this->id; };
+        void deposit(int amount);
+        void withdraw(int amount);
+        int throwDice(void);
+
+        Player& operator=(const Player& other);
 };
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,38 @@
 #include "Player.h"
+
+Player::Player(int amount, int pos, char idnum){
+    this->balance = amount;
+    this->position = pos;
+    this->id = idnum;
+}
+
+Player::Player(const Player& other){
+    this->balance = other.balance;
+    this->position = other.position;
+    this->id = other.id;
+}
+
+void Player::deposit(int amount){
+    this->balance += amount;
+}
+
+void Player::withdraw(int amount){
+    this->balance -= amount;
+}
+
+int Player::throwDice(void){
+    int roll;
+    srand(time(0));
+    for(int i = 0; i < 2; i++)
+        roll+= (rand() % 6 + 1);
+    return roll;
+
+}
+
+Player& Player::operator=(const Player& other){
+    this->balance = other.balance;
+    this->position = other.position;
+    this->id = other.id;
+
+    return *this;
+}
