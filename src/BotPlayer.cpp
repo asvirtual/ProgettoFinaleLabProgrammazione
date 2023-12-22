c45d06e348699fec53efb2c4ab8888bfcad8329d
Boskaa <andrea.boscarin@gmail.com>
2023-12-22 10:39:43 +0100

    Changed botPlayer probability to do action

--- src/BotPlayer.cpp
+++ src/BotPlayer.cpp
@@ -1,6 +1,6 @@
 #include "BotPlayer.h"
 
-bool BotPlayer::buyTile(SideTile* tile){
+bool BotPlayer::doAction(SideTile* tile){
     if (this->balance < tile->getPrice()) return false;
-    return rand() % 2;
+    return rand() % 4;
 }
