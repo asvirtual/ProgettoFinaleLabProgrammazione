0a758ed5ba7b3e3c84871d6a96c190f93cabe19a
Boskaa <andrea.boscarin@gmail.com>
2023-12-23 11:11:45 +0100

    Added argc and argv check on main

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 
 #include <memory>
 #include <vector>
+#include <string>
 
 int main(int argc, char *argv[]) {
 
-    if(argc != 1){
+    if(argc != 1 && (std::string(argv[1]) != "computer" && std::string(argv[1]) != "human")){
         std::cout <<"Inserisci human per giocare contro 3 bot o computer per guardare una partita tra 4 bot" << std::endl;
         return 1;
     }
