7a3dddd838eb65cf179cf5e0962e6a56f7c4e6b4
Asvirtual <didack.g@gmail.com>
2023-12-24 10:55:04 +0100

    Fixed argc argv check in main

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 int main(int argc, char *argv[]) {
     // Check if the user has provided a valid argument
-    if (argc != 1 && (std::string(argv[1]) != "computer" && std::string(argv[1]) != "human")) {
+    if (argc != 1 || ((std::string(argv[1]) != "computer" && std::string(argv[1]) != "human"))) {
         std::cerr << "Inserisci human per giocare contro 3 bot o computer per assistere a una partita tra 4 bot" << std::endl;
         throw std::invalid_argument("Invalid argument");
     }
