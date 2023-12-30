db0c6a3d59152ab5300221b0e46d66fd31db33d5
Asvirtual <didack.g@gmail.com>
2023-12-30 13:52:21 +0100

    Added missing comments

--- include/monopoly.h
+++ include/monopoly.h
@@ -16,8 +16,6 @@ typedef std::pair<Player*, int> playerRollPair;
 
 namespace monopUtil{
     char nthLetter(int idx);
-    // Sorts the players by their dice roll at the beginning of the game
-    void sortPlayers(std::vector<playerRollPair>& rolls, std::vector<playerRollPair>::iterator begin, std::vector<playerRollPair>::iterator end);
     void gameLoop(Board board); // Main game loop, determines the starting player and runs the game
     void openLogFile(void);
     void closeLogFile(void);
--- src/monopoly.cpp
+++ src/monopoly.cpp
@@ -13,71 +13,74 @@ char monopUtil::nthLetter(int idx)
 }
 
 /*
-    This function sorts the players by their dice roll at the beginning of the game.
+    This function gets the correct turn order of the players based on their dice roll at the beginning of the game.
     It takes a vector of pairs of pointers to players and their dice roll, and two iterators to the beginning and the end of a portion of the vector.
     The function will sort the players by their dice roll in non-increasing order and then, if there are players with the same dice roll
-    the function will recursively call itself to sort the sub-portion of the vector containing the players with the same dice roll.
+    the function will recursively call itself to sort the sub-portions of the vector containing the players with the same dice roll.
 */
-void monopUtil::sortPlayers(std::vector<playerRollPair>& rolls, std::vector<playerRollPair>::iterator begin, std::vector<playerRollPair>::iterator end) {
-    std::cout << "Sorting portion: [ ";
-    for (auto it = begin; it != end; it++) {
-        std::cout << std::to_string(it->first->getId()) << ", ";
-    }
-
-    std::cout << "]\n";
-
+void getTurnOrder(std::vector<playerRollPair>& rolls, std::vector<playerRollPair>::iterator begin, std::vector<playerRollPair>::iterator end) {
     // Throw the dice for each player
     for (auto it = begin; it != end; it++) it->second = it->first->throwDice();
     
-    // Sort the players by their dice roll in non-increasing order (if p1 is greater or equal than p2, p1 will be placed before p2)
+    // Sort the players by their dice roll in non-increasing order
     std::sort(begin, end, [] (auto p1, auto p2) { return p1.second > p2.second; });
-
-    // Print the temporary player order
-    std::cout << "[ ";
-    for (const playerRollPair& roll: rolls)
-        std::cout << "(" << std::to_string(roll.first->getId()) << ", " << std::to_string(roll.second) << ")" << ", ";
     
-    std::cout << "]\n";
-    
-    // Check if there are players with the same dice roll and, if so, keep track of the number of players with the same dice roll
-    // and recursively call this function to sort the sub-portions of the vector containing the players with the same dice roll
-    int nonSortedPortion = 0;
+    // Check if there are players with the same dice roll and, if so, recursively call this function 
+    // to sort the sub-portions of the vector containing the players with the same dice roll
+    int portionSize = 0;
     for (auto it = begin; it != end; it++) {
         // Keep track of the number of players with the same dice roll
-        if (it != end - 1 && it->second == (it + 1)->second) nonSortedPortion++; 
+        if (it != end - 1 && it->second == (it + 1)->second) portionSize++; 
 
-        // If we found a portion of the vector containing players with the same dice roll and we reached its end (the next player has 
-        // a different dice roll or the iterator points to the last element) sort the players in the said portion of the vector
-        if (nonSortedPortion > 0 && (it == end - 1 || it->second != (it + 1)->second)) {
-            sortPlayers(rolls, it - nonSortedPortion, it + 1);
-            nonSortedPortion = 0;
+        // If we found a sub-portion of the vector containing players with the same dice roll and we reached its end (the next player has 
+        // a different dice roll or the iterator points to the last element) sort the players in the said sub-portion of the vector
+        if (portionSize > 0 && (it == end - 1 || it->second != (it + 1)->second)) {
+            getTurnOrder(rolls, it - portionSize, it + 1);
+            portionSize = 0;
         }
     }
 }
 
-void monopUtil::gameLoop(Board board) {
-    // Initialize the rolls vector, made of pairs of pointers to players and their 
+/*
+    This function sorts the players based on the dice roll at the beginning of the game.
+    It uses the getTurnOrder function to sort the initialRolls vector by the players' dice roll 
+    and then it sorts the actual board.players vector
+*/
+void sortPlayers(std::vector<std::shared_ptr<Player>>& players) {
+    // Initialize the initialRolls vector, made of pairs of pointers to players and their 
     // dice roll, which we will then use to sort the actual board.players vector
-    std::vector<playerRollPair> rolls;
-    for (const std::shared_ptr<Player>& player: board.getPlayers())
-        rolls.push_back(playerRollPair(player.get(), 0));
+    std::vector<playerRollPair> initialRolls;
+    for (const std::shared_ptr<Player>& player: players) initialRolls.push_back(playerRollPair(player.get(), 0));
 
-    sortPlayers(rolls, rolls.begin(), rolls.end());
+    getTurnOrder(initialRolls, initialRolls.begin(), initialRolls.end());
 
-    // Sort the players vector based on the roll vector, by finding the dice roll of each player in the roll vector
-    for (auto it = rolls.begin(); it != rolls.end(); it++) {
+    // Sort the players vector based on the order found in initialRolls vector
+    for (auto it = players.begin(); it != players.end(); it++) {
+        // shared_ptr found in the current position of the players vector
+        std::shared_ptr<Player>& current = *it; 
+        // Pointer to the player found in the corresponding position of the initialRolls vector
+        Player* correspondingPlayer = initialRolls[it - players.begin()].first; 
+
+        // If the current player is already in the correct position, skip it
+        if (*current.get() == *correspondingPlayer) continue;
+
+        // Find the correct index for the "current" player, by finding the iterator pointing to the same player in the initialRolls vector
         auto toSwapIt = std::find_if(
-            board.players.begin(), board.players.end(),
-            [it] (const std::shared_ptr<Player>& p) { return *p == *it->first; }
+            initialRolls.begin(), initialRolls.end(), 
+            [it] (const playerRollPair& roll) { return *it->get() == *roll.first; }
         );
+        int toSwapIdx = toSwapIt - initialRolls.begin();
 
-        std::swap(board.players[it - rolls.begin()], board.players[toSwapIt - board.players.begin()]);
+        // Swap the pointer owned by the current shared_ptr with the pointer owned by the shared_ptr found at toSwapIdx
+        current.swap(players[toSwapIdx]);
     }
+}
 
+void monopUtil::gameLoop(Board board) {
+    sortPlayers(board.players);
+    log("Ordine di gioco: ");
     for (const std::shared_ptr<Player>& player: board.getPlayers())
-        log("Giocatore " + std::to_string(player->getId()));
-
-    return;
+        monopUtil::log("Giocatore " + std::to_string(player->getId()));
 
     int turn = 0;
     board.print();
@@ -88,11 +91,11 @@ void monopUtil::gameLoop(Board board) {
         for (const std::shared_ptr<Player>& player: board.getPlayers()) {
             if (player.get() == nullptr) continue; // Skip players that have lost and have been eliminated during this turn
             board.move(player); 
-            log("Giocatore " + std::to_string(player->getId()) + " ha finito il turno");
+            monopUtil::log("Giocatore " + std::to_string(player->getId()) + " ha finito il turno");
         }
     }
 
-    // Print the final standings and/or the game winner
+    // Print the final standings and/or the winner
     board.getFinalStandings();
 }
 
